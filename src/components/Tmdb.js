0dea1c7bca1a08af3b78b65673ac4c6469397231
alejandropascuale <apascuale@outlook.com>
2022-01-31 12:58:52 -0300

    fix problem with url to get movies

--- src/components/Tmdb.js
+++ src/components/Tmdb.js
@@ -1,5 +1,9 @@
-const basicFetch = async (endpoint) => {
-    const req = await fetch(`${process.env.TMDB_API_BASE}${endpoint}`);
+const newUrl = process.env.REACT_APP_TMDB_API_BASE4;
+const url = process.env.REACT_APP_TMDB_API_BASE3;
+const apiKey = process.env.REACT_APP_TMDB_API_Key;
+
+const basicFetch = async (url, endpoint) => {
+    const req = await fetch(`${url}${endpoint}`);
     const json = await req.json();
     return json;
 }
@@ -11,42 +15,42 @@ export default {
             {
                 slug: 'originals',
                 title: 'Originales de Netflix',
-                items: await basicFetch(`/discover/tv?with_network=213&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(url, `/discover/tv?with_network=213&languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para vos',
-                items: await basicFetch(`/trending/all/week?languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(url, `/trending/all/week?languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'toprated',
                 title: 'En alta',
-                items: await basicFetch(`/movie/top_rated?languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(url, `/movie/top_rated?languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'action',
                 title: 'Acción',
-                items: await basicFetch(`/discover/movie?with_genres=28&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(newUrl, `/discover/movie?with_genres=28&languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comedia',
-                items: await basicFetch(`/discover/movie?with_genres=35&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(newUrl, `/discover/movie?with_genres=35&languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(newUrl, `/discover/movie?with_genres=27&languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'romance',
                 title: 'Románticas',
-                items: await basicFetch(`/discover/movie?with_genres=10749&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(newUrl, `/discover/movie?with_genres=10749&languege=es-AR&api_key=${apiKey}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentales',
-                items: await basicFetch(`/discover/movie?with_genres=99&languege=es-AR&api_key=${process.env.TMDB_API_Key}`)
+                items: await basicFetch(newUrl, `/discover/movie?with_genres=99&languege=es-AR&api_key=${apiKey}`)
             }
         ];
     }
