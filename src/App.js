f513c4003e3597beeba85857721c81ffc6ba8e88
alejandropascuale <apascuale@outlook.com>
2022-01-31 16:54:13 -0300

    create styles in app and movie rows

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import MovieRow from './components/MovieRow';
 import Tmdb from './components/Tmdb';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
 
   useEffect( ()=> {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
     loadAll();
   }, [])
 
   return (
-    <>
-
-    </>
+    <div>
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+          ))}
+          {console.log(movieList)}
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/MovieRow.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/MovieRow.css'
+
+function MovieRow({ title, items }) {
+  return (
+    <div className='movieRow'>
+        <h2>{title}</h2>
+        <div className='movieRow--listarea'>
+          <div className='movieRow--list'>
+            {items.results.length > 0 && items.results.map((item, key) => (
+              <div className='movieRow--item' key={key}>
+                <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={`${item.name}`} />
+              </div>
+            ))}
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default MovieRow;
