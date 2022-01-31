ea9504bf46a76b6bfe0d402f7d6a5d8b28cb3011
alejandropascuale <apascuale@outlook.com>
2022-01-31 17:30:19 -0300

    create Feature Movie component with styles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import MovieRow from './components/MovieRow';
 import Tmdb from './components/Tmdb';
+import FeatureMovie from './components/FeatureMovie';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
   useEffect( ()=> {
     const loadAll = async () => {
+      // get all list of data
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Chose a random from originals from feature
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      setFeatureData(chosen);
     }
     loadAll();
   }, [])
 
   return (
-    <div>
+    <div className='page'>
+      {featureData &&
+        <FeatureMovie item={featureData} />
+      }
       <section className='lists'>
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeatureMovie.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/FeatureMovie.css'
+
+export default function FeatureMovie() {
+  return (
+    <>
+        
+    </>
+  )
+}
