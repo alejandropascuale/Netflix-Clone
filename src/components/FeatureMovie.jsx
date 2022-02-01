17a701fe84fcd4bbb4802013e11a853482eaadcb
alejandropascuale <apascuale@outlook.com>
2022-02-01 11:01:46 -0300

    complete info about feature movie in home

--- src/components/FeatureMovie.jsx
+++ src/components/FeatureMovie.jsx
@@ -4,6 +4,10 @@ import '../styles/FeatureMovie.css'
 export default function FeatureMovie({ item }) {
   console.log(item);
   let firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
 
   return (
     <section className='featured' 
@@ -17,14 +21,15 @@ export default function FeatureMovie({ item }) {
             <div className="featured--name">{item.name}</div>
             <div className="featured--info">
               <div className="featured--points">{item.vote_average} puntos</div>
-              <div className="featured-year">2099</div>
+              <div className="featured-year">{firstDate.getFullYear()}</div>
               <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1? 's': ''}</div>
             </div>
             <div className="featured--description">{item.overview}</div>
             <div className="featured--buttons">
-
+              <a href={`/watch/${item.id}`}>▶ Ver</a>
+              <a href={`/list/add/${item.id}`}>+ Agregar a mi lista</a>
             </div>
-            <div className="featured--genres"><strong>Géneros:</strong> ...</div>
+            <div className="featured--genres"><strong>Géneros:</strong> {genres.join(', ')}</div>
           </div>
         </div>
     </section>
