f6668bab941000ae820e68412092e1175b7c7e7d
alejandropascuale <apascuale@outlook.com>
2022-02-01 11:18:52 -0300

    progress with styles to feature movie in home

--- src/components/FeatureMovie.jsx
+++ src/components/FeatureMovie.jsx
@@ -21,7 +21,7 @@ export default function FeatureMovie({ item }) {
             <div className="featured--name">{item.name}</div>
             <div className="featured--info">
               <div className="featured--points">{item.vote_average} puntos</div>
-              <div className="featured-year">{firstDate.getFullYear()}</div>
+              <div className="featured--year">{firstDate.getFullYear()}</div>
               <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1? 's': ''}</div>
             </div>
             <div className="featured--description">{item.overview}</div>
